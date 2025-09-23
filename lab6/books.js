d6b769caf72ad3f1ca14824e07660709c6996243
Калинин Игорь <lapis-lines@list.ru>
2025-09-23 11:28:35 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,152 @@
+let books = [
+    {
+        title: "Быстрый способ бросить курить",
+        author: "Аллен Карр",
+        year: 1985,
+        genre: "Саморазвитие",
+        pages: 288,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Богатый папа, бедный папа",
+        author: "Роберт Кийосаки",
+        year: 1997,
+        genre: "Финансы",
+        pages: 352,
+        price: 600,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Война и мир",
+        author: "Лев Толстой",
+        year: 1869,
+        genre: "Классическая литература",
+        pages: 1225,
+        price: 800,
+        bestseller: true,
+        available: false
+    },
+    {
+        title: "Гарри Поттер и философский камень",
+        author: "Джоан Роулинг",
+        year: 1997,
+        genre: "Фэнтези",
+        pages: 432,
+        price: 550,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Мастер и Маргарита",
+        author: "Михаил Булгаков",
+        year: 1967,
+        genre: "Классическая литература",
+        pages: 480,
+        price: 500,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Преступление и наказание",
+        author: "Фёдор Достоевский",
+        year: 1866,
+        genre: "Классическая литература",
+        pages: 608,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "1984",
+        author: "Джордж Оруэлл",
+        year: 1949,
+        genre: "Антиутопия",
+        pages: 320,
+        price: 400,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Три товарища",
+        author: "Эрих Мария Ремарк",
+        year: 1936,
+        genre: "Роман",
+        pages: 480,
+        price: 520,
+        bestseller: true,
+        available: false
+    },
+    {
+        title: "Маленький принц",
+        author: "Антуан де Сент-Экзюпери",
+        year: 1943,
+        genre: "Философская сказка",
+        pages: 96,
+        price: 350,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Атлант расправил плечи",
+        author: "Айн Рэнд",
+        year: 1957,
+        genre: "Философский роман",
+        pages: 1368,
+        price: 1200,
+        bestseller: false,
+        available: true
+    },
+    {
+        title: "Сто лет одиночества",
+        author: "Габриэль Гарсиа Маркес",
+        year: 1967,
+        genre: "Магический реализм",
+        pages: 448,
+        price: 580,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Властелин колец",
+        author: "Дж. Р. Р. Толкин",
+        year: 1954,
+        genre: "Фэнтези",
+        pages: 1137,
+        price: 950,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Анна Каренина",
+        author: "Лев Толстой",
+        year: 1877,
+        genre: "Классическая литература",
+        pages: 864,
+        price: 650,
+        bestseller: true,
+        available: true
+    },
+    {
+        title: "Унесённые ветром",
+        author: "Маргарет Митчелл",
+        year: 1936,
+        genre: "Исторический роман",
+        pages: 1024,
+        price: 700,
+        bestseller: true,
+        available: false
+    },
+    {
+        title: "Шантарам",
+        author: "Грегори Дэвид Робертс",
+        year: 2003,
+        genre: "Роман",
+        pages: 874,
+        price: 750,
+        bestseller: true,
+        available: true
+    }
+];
